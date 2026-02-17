7802f29659476ab2b18c7927cb80f5e048f20898
pat <pat@net-dev01.myguest.virtualbox.org>
2026-02-18 05:01:54 +0700

    Update Terraform project src

---
+++ 02-Cloud-infrastructure/Terraform/02-homeworks/src/vms_platform.tf
@@ -0,0 +1,57 @@
+###db vm vars
+
+variable "vm_db_name" {
+  type    = string
+  default = "netology-develop-platform-db"
+}
+
+variable "vm_db_platform_id" {
+  type    = string
+  default = "standard-v3"
+}
+
+variable "vm_db_cores" {
+  type    = number
+  default = 2
+}
+
+variable "vm_db_memory" {
+  type    = number
+  default = 2
+}
+
+variable "vm_db_core_fraction" {
+  type    = number
+  default = 20
+}
+
+variable "vm_db_preemptible" {
+  type    = bool
+  default = true
+}
+
+variable "vm_db_nat" {
+  type    = bool
+  default = true
+}
+
+variable "vm_db_serial_port_enable" {
+  type    = number
+  default = 1
+}
+
+variable "subnet_db_zone" {
+  type    = string
+  default = "ru-central1-b"
+}
+
+variable "subnet_db_name" {
+  type    = string
+  default = "develop_db"
+}
+
+variable "subnet_db_cidr" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
